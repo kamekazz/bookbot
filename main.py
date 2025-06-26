8f857fb0d2b8a81507ecfc0bb4cec1f1f87bf308
kamekazz <taveras78@gmail.com>
2025-06-26 19:50:16 -0400

    a description of your changes here

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from stats import get_num_of_words , count_characters
+from stats import get_num_of_words , count_characters, convert_dic_list
+import sys
 
 def get_book_text(_filepath):
-    with open(f"books/{_filepath}") as f:
+    with open(f"{_filepath}") as f:
         file_contents = f.read()
         return file_contents
 
@@ -9,9 +10,26 @@ def get_book_text(_filepath):
 
 
 def main():
-    book_date = get_book_text('frankenstein.txt')
+      # 1) Make sure we got exactly one argument (the book path)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+
+    print('============ BOOKBOT ============')
+    print(f"Analyzing book found at {book_path}...")
+    book_date = get_book_text(book_path)
+    print('----------- Word Count ----------')
     word_counts = count_characters(book_date)
-    print( word_counts)
-    #print(f"{get_num_of_words(book_date)} words found in the document")
+    print(f"Found {get_num_of_words(book_date)} total words")
+    print('--------- Character Count -------')
+    list_of_char = convert_dic_list(word_counts)
+    for char in list_of_char:
+        c =char['char']
+        n =char['num']
+        print(f'{c}: {n}')
+    print("============= END ===============")
     
 main()
+
--- stats.py
+++ stats.py
@@ -2,9 +2,17 @@ def get_num_of_words(_text):
     return len(_text.split())
 
 def count_characters (_data)-> dict[str,int]:
-    obj ={}
+    objs ={}
     for l in _data.lower():
-        obj[l] = obj.get(l,0) + 1
-    return obj
+        objs[l] = objs.get(l,0) + 1
+    return objs
 
 
+
+def convert_dic_list(objs: dict[str,int]) -> list[dict[str,int]]:
+    lst = [{"char": c, "num": n} for c, n in objs.items()]
+    lst.sort(key=lambda item: item["num"], reverse=True)
+    return lst
+
+def create_dic(_char,_num)-> dict:
+    return {"char",_char,"num",_num}
